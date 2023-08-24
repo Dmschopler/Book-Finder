2fad520696c6e1ff46a29ef30d9ce77e44f72b92
Daniel S <dmschopler@gmail.com>
2023-08-24 18:10:20 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
-const { authMiddleware } = require("./middleware/auth");
+const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
